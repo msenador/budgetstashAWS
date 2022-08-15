ca817fdec15a61f5d97d47b623c344910bed3df7
msenador <smorian@yahoo.com>
2022-08-15 02:01:42 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useContext, useEffect } from 'react';
 import { Box } from '@mui/system';
-import { Routes, Route } from 'react-router-dom';
-import Appbar from './components/Appbar';
-import Home from './pages/Home';
+// import { Routes, Route } from 'react-router-dom';
+// import Appbar from './components/Appbar';
+// import Home from './pages/Home';
 import UserContext from './context/UserContext';
-import MemberContent from './pages/MemberContent';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import HomeMobile from './mobile/HomeMobile';
+// import MemberContent from './pages/MemberContent';
+// import useMediaQuery from '@mui/material/useMediaQuery';
+// import HomeMobile from './mobile/HomeMobile';
 
 const App = () => {
-  const matches = useMediaQuery('(min-width:600px)');
+  // const matches = useMediaQuery('(min-width:600px)');
   const { currentUser, setCurrentUser } = useContext(UserContext);
 
   useEffect(() => {
@@ -23,13 +23,14 @@ const App = () => {
 
   return (
     <Box>
-      {currentUser.username && <Appbar />}
+      {/* {currentUser.username && <Appbar />}
       <Routes>
         <Route
           path="/"
           element={currentUser.username ? <MemberContent /> : matches ? <Home /> : <HomeMobile />}
         />
-      </Routes>
+      </Routes> */}
+      hi
     </Box>
   );
 };
