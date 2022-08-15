4a90b43c5efdd20d790a734a5cf3e6ca79183c56
msenador <smorian@yahoo.com>
2022-08-15 02:08:43 -0400

    refix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useContext, useEffect } from 'react';
 import { Box } from '@mui/system';
-// import { Routes, Route } from 'react-router-dom';
-// import Appbar from './components/Appbar';
-// import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import Appbar from './components/Appbar';
+import Home from './pages/Home';
 import UserContext from './context/UserContext';
-// import MemberContent from './pages/MemberContent';
-// import useMediaQuery from '@mui/material/useMediaQuery';
-// import HomeMobile from './mobile/HomeMobile';
+import MemberContent from './pages/MemberContent';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import HomeMobile from './mobile/HomeMobile';
 
 const App = () => {
-  // const matches = useMediaQuery('(min-width:600px)');
+  const matches = useMediaQuery('(min-width:600px)');
   const { currentUser, setCurrentUser } = useContext(UserContext);
 
   useEffect(() => {
     if (currentUser) {
+      console.log(currentUser);
       localStorage.setItem('userEmail', JSON.stringify(currentUser));
       setCurrentUser(currentUser);
     } else {
@@ -23,14 +24,13 @@ const App = () => {
 
   return (
     <Box>
-      {/* {currentUser.username && <Appbar />}
+      {currentUser.username && <Appbar />}
       <Routes>
         <Route
           path="/"
           element={currentUser.username ? <MemberContent /> : matches ? <Home /> : <HomeMobile />}
         />
-      </Routes> */}
-      hi
+      </Routes>
     </Box>
   );
 };
